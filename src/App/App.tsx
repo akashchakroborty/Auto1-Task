50b2941a0ecaa9b55c3606a04f48d623fc0629fe
Akash Chakroborty <akashchakroborty@Akashs-MacBook-Pro.local>
2022-01-20 16:39:10 +0530

    Adding Footer

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,6 +3,7 @@ import Theme from '../ThemeProvider';
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <CssBaseline />
       <Router>
         <Header />
+        <Footer />
       </Router>
     </Theme>
   );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { makeStyles, createStyles } from '@mui/styles';
+import { Theme, Typography } from '@mui/material';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    footer: {
+      borderTop: `1px solid ${theme.palette.divider}`,
+      marginTop: 64,
+      padding: 16,
+      height: 80,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  }),
+);
+
+const Footer = () => {
+  const classes = useStyles();
+  return (
+    <footer className={classes.footer}>
+      <Typography variant="body2" color="textSecondary" align="center">
+        &copy; AUTO1 Group 2022
+      </Typography>
+    </footer>
+  );
+};
+
+export default Footer;
